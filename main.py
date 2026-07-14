5772eeb02ecba051dae1b61483035d1a0be95804
yours-truly68 <mohammedrazim880@gmail.com>
2026-07-15 01:19:27 +0530

    Refactor: Replace main function with FastAPI application factory for the Forge platform

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from coding-agent!")
+from fastapi import FastAPI
+import inngest.fast_api
+from harness.queue import inngest_client, run_durable_agent_loop
 
 
-if __name__ == "__main__":
-    main()
+def create_app() -> FastAPI:
+    """
+    The Application Factory. Responsible for building, configuring, and initalising the running state of the Forge Platform.
+    """
+    
+    app = FastAPI(
+        title = "Forge: Stateless Coding Agent Platform",
+        version = "0.1.0"
+    )
+    
+    
+    inngest.fast_api.serve(
+        app,
+        inngest_client,
+        [run_durable_agent_loop]
+    )
+    
+    
+    return app
+
+
+app = create_app()
